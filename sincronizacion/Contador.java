3d64c88d0b400c9b85295959796360f1f6441edb
hectorpailacura-oss <https://github.com/hectorpailacura-oss/estoy.git>
2025-08-22 14:58:29 -0300

    tester

---
+++ sincronizacion/Contador.java
@@ -0,0 +1,14 @@
+package sincronizacion;
+
+public class Contador {
+
+	String nombre ;
+	
+	private int valor =100;
+	public void incrementar() {++valor;}
+		public void increnementarCon(int cant) {this.valor=valor+ cant;}
+		public int getCant() {return valor;}
+	}
+	
+	
+
---
+++ sincronizacion/RunnableContador.java
@@ -0,0 +1,22 @@
+package sincronizacion;
+
+public class RunnableContador implements Runnable {
+
+  Contador elCon;
+  public RunnableContador(Contador con) {
+	  this.elCon=con;}
+ 
+  
+  public void run() {
+		  
+		  for(int x=0; x<1000;++x) {
+			  this.elCon.incrementar();}
+		  
+	  }
+	  
+  }
+	
+
+	
+
+
