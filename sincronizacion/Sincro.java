a8882dba104f14ae5a807b982ddbafffdb221c86
hectorpailacura-oss <https://github.com/hectorpailacura-oss/estoy.git>
2025-08-22 13:43:39 -0300

    al infinito y mas

---
+++ sincronizacion/Sincro.java
@@ -0,0 +1,10 @@
+package sincronizacion;
+
+public class Sincro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ sincronizacion2/Sincro2.java
@@ -0,0 +1,10 @@
+package sincronizacion2;
+
+public class Sincro2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
